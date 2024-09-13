5f48fdf81bb90f3f050a159428d0259ecb3c9a19
debasis <debasispanda3454@gmail.com>
2024-09-13 13:55:58 +0530

    feat: add-files

---
+++ resources/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.9.5"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.2.2"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.53.1"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.1.0"
+    }
+  }
+}
