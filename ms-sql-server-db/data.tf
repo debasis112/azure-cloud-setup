944a7e2c9258acb17aeddf8881274c6cb6adbc34
debasis <debasispanda3454@gmail.com>
2024-09-16 12:05:35 +0530

    feat: add-data-for-sql-db

--- ms-sql-server-db/data.tf
+++ ms-sql-server-db/data.tf
@@ -3,6 +3,15 @@ data "azurerm_resource_group" "rsg-01" {
 }
 
 data "azurerm_mssql_server" "sql-server-01" {
-  name                = "debasis-project-mssqlserver-01"
+  name                = var.mssqlserver_name
   resource_group_name = data.azurerm_resource_group.rsg-01.name
+}
+
+data "azurerm_mssql_database" "sql-database-01" {
+  name      = var.mssqldatabase_name
+  server_id = azurerm_mssql_server.sql-server-01.id
+}
+
+output "database_id" {
+  value = data.azurerm_mssql_database.sql-database-01.id
 }
