2b4d02eb104dd11a06539261a4a14c897fbca661
debasis <debasispanda3454@gmail.com>
2024-09-16 08:12:26 +0530

    feat: create-sql-server

--- ms-sql-server-db/data.tf
+++ ms-sql-server-db/data.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "rsg-01" {
   name = "debasis-project-rsg-01"
 }
 
-data "azurerm_mssql_server" "sql-server-01" {
-  name                = "debasis-project-mssqlserver-01"
-  resource_group_name = data.azurerm_resource_group.rsg-01.name
-}
+# data "azurerm_mssql_server" "sql-server-01" {
+#   name                = "debasis-project-mssqlserver-01"
+#   resource_group_name = data.azurerm_resource_group.rsg-01.name
+# }
