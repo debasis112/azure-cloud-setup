c9559df0bb17b7fb027479307043e04707c4dbc1
debasis <debasispanda3454@gmail.com>
2024-09-16 07:04:17 +0530

    feat: added-data-output-tf-file

---
+++ ms-sql-server-db/ms-sql-server.tf
@@ -0,0 +1,6 @@
+# # Create a ms sql server
+# resource "azurerm_resource_group" "rsg-01" {
+#   name     = "debasis-project-rsg-01"
+#   location = "Central India" # or "South India" or "West India"
+#   tags     = local.common_tags
+# }
---
+++ resource-group/output.tf
@@ -0,0 +1,11 @@
+output "rsg-01-id" {
+  value = data.azurerm_resource_group.rsg-01.id
+}
+
+output "rsg-01-location" {
+  value = data.azurerm_resource_group.rsg-01.location
+}
+
+output "rsg-01-tags" {
+  value = data.azurerm_resource_group.rsg-01.tags
+}
