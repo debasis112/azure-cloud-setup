15d16061898045c299d088efb98c973d23d8028a
debasis <debasispanda3454@gmail.com>
2024-09-16 08:03:52 +0530

    feat: create-sql-server

--- ms-sql-server-db/ms-sql-server.tf
+++ ms-sql-server-db/ms-sql-server.tf
@@ -1,17 +1,17 @@
-# # Create a ms sql server
-# resource "azurerm_mssql_server" "sql-server-01" {
-#   name                         = "debasis-project-mssqlserver-01"
-#   resource_group_name          = azurerm_resource_group.rsg-01.name
-#   location                     = azurerm_resource_group.rsg-01.location
-#   version                      = "12.0"
-#   administrator_login          = var.mssqlserver_admin_name
-#   administrator_login_password = var.mssqlserver_admin_pass
-#   minimum_tls_version          = "1.2"
+# Create a ms sql server
+resource "azurerm_mssql_server" "sql-server-01" {
+  name                         = "debasis-project-mssqlserver-01"
+  resource_group_name          = azurerm_resource_group.rsg-01.name
+  location                     = azurerm_resource_group.rsg-01.location
+  version                      = "12.0"
+  administrator_login          = var.mssqlserver_admin_name
+  administrator_login_password = var.mssqlserver_admin_pass
+  minimum_tls_version          = "1.2"
 
-#   azuread_administrator {
-#     login_username = var.ad_admin_name
-#     object_id      = var.ad_admin_obj_id // Debasis Panda
-#   }
+  azuread_administrator {
+    login_username = var.ad_admin_name
+    object_id      = var.ad_admin_obj_id // Debasis Panda
+  }
 
-#   tags = local.common_tags
-# }
+  tags = local.common_tags
+}
