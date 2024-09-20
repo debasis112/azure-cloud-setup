73fa84d406b52e1f3c7006ef3d900c2da733ddc2
debasis <debasispanda3454@gmail.com>
2024-09-21 02:04:09 +0530

    feat: delete-sql-components

--- ms-sql-server-db/data.tf
+++ ms-sql-server-db/data.tf
@@ -2,12 +2,12 @@ data "azurerm_resource_group" "rsg-01" {
   name = "debasis-project-rsg-01"
 }
 
-data "azurerm_mssql_server" "sql-server-01" {
-  name                = var.mssqlserver_name
-  resource_group_name = data.azurerm_resource_group.rsg-01.name
-}
+# data "azurerm_mssql_server" "sql-server-01" {
+#   name                = var.mssqlserver_name
+#   resource_group_name = data.azurerm_resource_group.rsg-01.name
+# }
 
-data "azurerm_mssql_database" "sql-database-01" {
-  name      = var.mssqldatabase_name
-  server_id = azurerm_mssql_server.sql-server-01.id
-}
+# data "azurerm_mssql_database" "sql-database-01" {
+#   name      = var.mssqldatabase_name
+#   server_id = azurerm_mssql_server.sql-server-01.id
+# }
--- ms-sql-server-db/variables.tf
+++ ms-sql-server-db/variables.tf
@@ -18,32 +18,32 @@ variable "tenant_id" {
   default = ""
 }
 
-variable "mssqlserver_name" {
-  type    = string
-  default = "debasis-project-mssqlserver-01"
-}
+# variable "mssqlserver_name" {
+#   type    = string
+#   default = "debasis-project-mssqlserver-01"
+# }
 
-variable "mssqlserver_admin_name" {
-  type    = string
-  default = ""
-}
+# variable "mssqlserver_admin_name" {
+#   type    = string
+#   default = ""
+# }
 
-variable "mssqlserver_admin_pass" {
-  type    = string
-  default = ""
-}
+# variable "mssqlserver_admin_pass" {
+#   type    = string
+#   default = ""
+# }
 
-variable "ad_admin_name" {
-  type    = string
-  default = ""
-}
+# variable "ad_admin_name" {
+#   type    = string
+#   default = ""
+# }
 
-variable "ad_admin_obj_id" {
-  type    = string
-  default = ""
-}
+# variable "ad_admin_obj_id" {
+#   type    = string
+#   default = ""
+# }
 
-variable "mssqldatabase_name" {
-  type    = string
-  default = "debasis-project-mssql-db-01"
-}
+# variable "mssqldatabase_name" {
+#   type    = string
+#   default = "debasis-project-mssql-db-01"
+# }
