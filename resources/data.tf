d2c2de78ab8dbe746b9950a02d5767fdb1ddbd70
debasis <debasispanda3454@gmail.com>
2024-09-22 19:30:34 +0530

    feat: sql-server

--- resources/data.tf
+++ resources/data.tf
@@ -1,6 +1,6 @@
-# data "azurerm_resource_group" "rsg-01" {
-#   name = "debasis-project-rsg-01"
-# }
+data "azurerm_resource_group" "rsg-01" {
+  name = var.rg_name
+}
 
 data "azurerm_mssql_server" "sql-server-01" {
   name                = var.mssqlserver_name
