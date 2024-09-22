2d25965d94032216950a9d651667dbcb34cbd24b
debasis <debasispanda3454@gmail.com>
2024-09-22 19:53:19 +0530

    feat: sql-database-create

--- resources/main.tf
+++ resources/main.tf
@@ -23,21 +23,21 @@ resource "azurerm_mssql_server" "sql-server-01" {
 }
 
 // MS SQL DATABASE
-# resource "azurerm_mssql_database" "sql-database-01" {
-#   name         = var.mssqldatabase_name
-#   server_id    = azurerm_mssql_server.sql-server-01.id
-#   // collation    = "SQL_Latin1_General_CP1_CI_AS"
-#    collation = "GP_S_Gen5_1"
-#   // license_type = "LicenseIncluded"
-#   max_size_gb  = 32
+resource "azurerm_mssql_database" "sql-database-01" {
+  name         = var.mssqldatabase_name
+  server_id    = azurerm_mssql_server.sql-server-01.id
+  // collation    = "SQL_Latin1_General_CP1_CI_AS"
+   collation = "GP_S_Gen5_1"
+  // license_type = "LicenseIncluded"
+  max_size_gb  = 20
 
-#   tags = local.common_tags
+  tags = local.common_tags
 
-#   # prevent the possibility of accidental data loss
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
 // MS SQL DATABASE Firewall
 # # // Database firewall rule-01
