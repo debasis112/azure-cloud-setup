5929cf144833706e5d888f27a99c2717e7ddd5c4
debasis <debasispanda3454@gmail.com>
2024-09-22 19:38:13 +0530

    feat: minor-update-data-tf

--- resources/main.tf
+++ resources/main.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "rsg-01" {
 // MS SQL server
 resource "azurerm_mssql_server" "sql-server-01" {
   name                         = var.mssqlserver_name
-  resource_group_name          = data.azurerm_resource_group.rsg-01.name
-  location                     = data.azurerm_resource_group.rsg-01.location
+  resource_group_name          = azurerm_resource_group.rsg-01.name
+  location                     = azurerm_resource_group.rsg-01.location
   version                      = "12.0"
   administrator_login          = var.mssqlserver_admin_name
   administrator_login_password = var.mssqlserver_admin_pass
