7a090e97589124acd476e28b0cef44f17efb6a73
debasis <debasispanda3454@gmail.com>
2024-09-22 18:52:19 +0530

    feat: file updates

--- resources/data.tf
+++ resources/data.tf
@@ -2,10 +2,6 @@ data "azurerm_resource_group" "rsg-01" {
   name = "debasis-project-rsg-01"
 }
 
-data "azurerm_resource_group" "rsg-01" {
-  name = "debasis-project-rsg-01"
-}
-
 data "azurerm_mssql_server" "sql-server-01" {
   name                = var.mssqlserver_name
   resource_group_name = data.azurerm_resource_group.rsg-01.name
