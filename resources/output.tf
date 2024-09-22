4873e69fbd89a06e3c8a3652d3702054d0863ba3
debasis <debasispanda3454@gmail.com>
2024-09-22 19:13:36 +0530

    feat: update

--- resources/output.tf
+++ resources/output.tf
@@ -1,23 +1,23 @@
-output "rsg-01-location" {
-  value = data.azurerm_resource_group.rsg-01.location
-}
-
-# output "rsg-01-tags" {
-#   value = data.azurerm_resource_group.rsg-01.tags
+# output "rsg-01-location" {
+#   value = data.azurerm_resource_group.rsg-01.location
 # }
 
-output "rsg-01-name" {
-  value = data.azurerm_resource_group.rsg-01.name
-}
+# # output "rsg-01-tags" {
+# #   value = data.azurerm_resource_group.rsg-01.tags
+# # }
 
-output "sql-server-01-id" {
-  value = data.azurerm_mssql_server.sql-server-01.id
-}
-
-output "sql-server-01-name" {
-  value = data.azurerm_mssql_server.sql-server-01.name
-}
-
-# output "sql-server-01-fqdn" {
-#   value = data.azurerm_mssql_server.sql-server-01.fully_qualified_domain_name
+# output "rsg-01-name" {
+#   value = data.azurerm_resource_group.rsg-01.name
 # }
+
+# output "sql-server-01-id" {
+#   value = data.azurerm_mssql_server.sql-server-01.id
+# }
+
+# output "sql-server-01-name" {
+#   value = data.azurerm_mssql_server.sql-server-01.name
+# }
+
+# # output "sql-server-01-fqdn" {
+# #   value = data.azurerm_mssql_server.sql-server-01.fully_qualified_domain_name
+# # }
