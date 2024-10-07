017117b3f444c27d62e7371349377238c68111f6
debasis <debasispanda3454@gmail.com>
2024-10-08 02:22:07 +0530

    feat: delete-all

--- resources/data.tf
+++ resources/data.tf
@@ -11,8 +11,3 @@
 #   name      = azurerm_mssql_database.sql-database-01.name
 #   server_id = azurerm_mssql_server.sql-server-01.id
 # }
-
-data "azurerm_kubernetes_cluster" "kbcl-01" {
-  name                = azurerm_kubernetes_cluster.kbcl-01.name
-  resource_group_name = azurerm_resource_group.rsg-01.name
-}
