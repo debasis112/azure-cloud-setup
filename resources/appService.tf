2e6d5cbbcfa018c5b5dd69b5cdd4c9451b3bbf2d
debasis <debasispanda3454@gmail.com>
2024-10-17 19:43:23 +0530

    feat: test-app-service-03

--- resources/appService.tf
+++ resources/appService.tf
@@ -7,16 +7,13 @@ resource "azurerm_linux_web_app" "app-service-01" {
   https_only          = true
 
   site_config {
-    # Correct format for specifying the Docker image
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/project-work:v1.0.0"
-    
-    # Enable HTTP2 support (optional)
-    http2_enabled = false
+    always_on = false
   }
 
   app_settings = {
     # Correctly setting the ACR connection details
-    "docker_registry_url"      = "https://${azurerm_container_registry.acr.login_server}"
+    "docker_registry_url"      = "DOCKER|${azurerm_container_registry.acr.login_server}"
+    "docker_image"             = "project-work:v1.0.0"
     "docker_registry_username" = azurerm_container_registry.acr.admin_username
     "docker_registry_password" = azurerm_container_registry.acr.admin_password
   }
