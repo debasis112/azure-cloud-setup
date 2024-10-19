8fddadcaf742fa694119b52446cfc6dfc33453fe
debasis <debasispanda3454@gmail.com>
2024-10-19 15:05:46 +0530

    feat: create-all-resources

--- resources/ms-sql-database-fw.tf
+++
@@ -1,7 +0,0 @@
-# # Define MS SQL DATABASE Firewall
-# resource "azurerm_mssql_firewall_rule" "sql-database-fw-01" {
-#   name             = "AllowAppService"
-#   server_id        = azurerm_mssql_server.sql-server-01.id
-#   start_ip_address = "0.0.0.0"
-#   end_ip_address   = "255.255.255.255"
-# }
