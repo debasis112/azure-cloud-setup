f0a42e5ba59a4f21eebbb0678950e673f4ace7e4
debasis <debasispanda3454@gmail.com>
2024-12-05 20:46:05 +0530

    feat: arc-machine-create

--- resources/08-arc-machine.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_arc_machine" "arc-machine-01" {
-  name                = "deb-arcmachine"
-  resource_group_name = azurerm_resource_group.rsg-01.name
-  location            = azurerm_resource_group.rsg-01.location
-  kind                = "SCVMM"
-}
