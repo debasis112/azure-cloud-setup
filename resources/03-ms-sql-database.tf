b6865aa22894c398681cad37e66c127f4139c92a
debasis <debasispanda3454@gmail.com>
2024-12-10 00:16:40 +0530

    feat: sql-database-create

--- resources/03-ms-sql-database.tf
+++ resources/03-ms-sql-database.tf
@@ -16,7 +16,7 @@
 module "sql_db-01" {
   source                  = "./modules/sql-database" # Replace with the path to your module folder
   sql_db_name             = var.sql_db_name
-  sql_db_server_id        = module.sql_server.sql_server-01.server_id
+  sql_db_server_id        = module.sql_server-01.sql_server_id
   sql_db_server_collation = var.sql_db_server_collation
   sql_db_server_gb_size   = var.sql_db_server_gb_size
   tags                    = var.tags
