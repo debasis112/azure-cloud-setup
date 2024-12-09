d8afc5bc66ecba9e83dc86d405018101753bca5b
debasis <debasispanda3454@gmail.com>
2024-12-09 22:05:32 +0530

    feat: module-create-test-01

--- resources/02-ms-sql-server.tf
+++ resources/02-ms-sql-server.tf
@@ -14,31 +14,16 @@
 #   }
 #   tags = local.common_tags
 # }
-
 module "mssql_server" {
-  source                       = "./1_sql_modules/modules/sql_server"
-  name                         = var.mssqlserver_name
-  resource_group_name          = azurerm_resource_group.rsg-01.name
-  location                     = azurerm_resource_group.rsg-01.location
-  version                      = "12.0"
-  administrator_login          = var.mssqlserver_admin_name
-  administrator_login_password = var.mssqlserver_admin_pass
-  minimum_tls_version          = "1.2"
-  ad_administrator_login       = var.ad_admin_name
-  ad_administrator_object_id   = var.ad_admin_obj_id
-  tags                         = local.common_tags
+  source                  = "./modules/mssql_server"
+  sql_server_name         = var.mssqlserver_name
+  sql_server_resg_name    = azurerm_resource_group.rsg-01.name
+  sql_server_location     = azurerm_resource_group.rsg-01.location
+  sql_server_version      = "12.0"
+  sql_server_admin_login  = var.mssqlserver_admin_name
+  sql_server_admin_login_password = var.mssqlserver_admin_pass
+  sql_server_minimum_tls_ver = "1.2"
+  ad_admin_name           = var.ad_admin_name
+  ad_admin_obj_id         = var.ad_admin_obj_id
+  tags                    = local.common_tags
 }
-
-# module "mssql_server" {
-#   source                       = "./modules/mssql_server"
-#   name                         = var.mssqlserver_name
-#   resource_group_name          = azurerm_resource_group.rsg-01.name
-#   location                     = azurerm_resource_group.rsg-01.location
-#   version                      = "12.0"
-#   administrator_login          = var.mssqlserver_admin_name
-#   administrator_login_password = var.mssqlserver_admin_pass
-#   minimum_tls_version          = "1.2"
-#   ad_administrator_login       = var.ad_admin_name
-#   ad_administrator_object_id   = var.ad_admin_obj_id
-#   tags                         = local.common_tags
-# }
