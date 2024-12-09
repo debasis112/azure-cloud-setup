b32a374c6231d8e61879f31e74ce64cc6b9f0023
debasis <debasispanda3454@gmail.com>
2024-12-09 22:55:22 +0530

    feat: module-create-test-01

--- resources/01-resource-group.tf
+++ resources/01-resource-group.tf
@@ -5,8 +5,21 @@
 #   tags     = local.common_tags
 # }
 
-module "rsg-01" {
-  source   = "./modules/resource-group"
+# module "rsg-01" {
+#   source   = "./modules/resource-group"
+#   name     = var.rg_name
+#   location = var.rg_location
+#   tags = {
+#     Environment = "Production"
+#     Department  = "cloud-computing"
+#     Owner       = "Debasis_Panda"
+#     Company     = "WIPRO"
+#     STD_ID      = "2020WA86256"
+#   }
+# }
+
+module "resource_group" {
+  source   = "./modules/resource-group" # Replace with the path to your module folder
   name     = var.rg_name
   location = var.rg_location
   tags = {
