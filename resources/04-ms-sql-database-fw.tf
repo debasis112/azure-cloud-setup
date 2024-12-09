53750079e6d1e8e3d1a42c39f6521dd6296cce3b
debasis <debasispanda3454@gmail.com>
2024-12-10 00:28:47 +0530

    db-fw

--- resources/04-ms-sql-database-fw.tf
+++ resources/04-ms-sql-database-fw.tf
@@ -2,7 +2,7 @@
 module "sql_db-fw-01" {
   source                 = "./modules/sql-database-fw" # Replace with the path to your module folder
   sql_db_fw_name         = var.sql_db_fw_name
-  sql_db_fw_server_id    = module.sql_server-01.sql_server_id
+  sql_db_fw_server_id    = module.sql_server.sql_server_id
   sql_db_fw_start_ip_add = var.sql_db_fw_start_ip_add
   sql_db_fw_end_ip_add   = var.sql_db_fw_end_ip_add
 
