50a3e86917b197b7c4150414dcacbba470927dc0
debasis <debasispanda3454@gmail.com>
2024-12-11 21:30:57 +0530

    feat: test-01-acr

--- resources/05-acr.tf
+++ resources/05-acr.tf
@@ -1,11 +1,11 @@
-# # Define ARC for image store
-# module "acr-01" {
-#   source            = "./modules/acr" # Path to the ACR module
-#   acr_count         = var.acr_count   # Pass the acr_count variable from the root
-#   acr_name          = var.acr_name    # Base name for ACR
-#   rg_name           = var.rg_name
-#   location          = var.rg_location
-#   acr_sku           = var.acr_sku
-#   acr_admin_enabled = var.acr_admin_enabled
-#   tags              = var.tags
-# }
+# Define ARC for image store
+module "acr-01" {
+  source            = "./modules/acr" # Path to the ACR module
+  acr_count         = "${var.acr_name}${count.index+1}"  # Pass the acr_count variable from the root
+  acr_name          = var.acr_name    # Base name for ACR
+  rg_name           = var.rg_name
+  location          = var.rg_location
+  acr_sku           = var.acr_sku
+  acr_admin_enabled = var.acr_admin_enabled
+  tags              = var.tags
+}
